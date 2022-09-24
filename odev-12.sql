4cc9df68c7bdec202793143aeffb4849b42d4f50
Mahmut Burak Aydin <109317317+mahmutburakaydin@users.noreply.github.com>
2022-09-24 17:42:21 +0300

    Create odev-12.sql

---
+++ odev-12.sql
@@ -0,0 +1,13 @@
+--1
+SELECT COUNT(*) FROM film 
+WHERE length > (SELECT AVG(length) FROM film);
+--2
+SELECT COUNT(*) FROM film
+WHERE rental_rate = (SELECT MAX(rental_rate) FROM film);
+--3
+SELECT title,rental_rate,replacement_cost FROM film
+WHERE rental_rate=(SELECT MIN(rental_rate)FROM film)AND replacement_cost=(SELECT MIN(replacement_cost)FROM film);
+--4
+SELECT customer.first_name, customer.last_name FROM customer
+JOIN payment On customer.customer_id = ANY
+(SELECT customer_id FROM payment GROUP BY customer_id ORDER BY COUNT(customer_id) DESC LIMIT 5) LIMIT 5;
